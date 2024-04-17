92551f46040e27d5f9a99c4fcff7c75d8ede5105
Nawaf Alshalan <noufi02@icloud.com>
2024-04-17 09:57:40 -0400

    Updated Error Handling

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // TODO: Enable strict mode
+"use strict";
 
 // TODO: Fix the following parameter list
-function parseToJSON(data, data) {
+function parseToJSON(data) {
   // TODO: Add a try/catch block to
   // attempt to convert 'data' to JSON
-  return JSON.parse(data); 
-  
+  try {
+    return JSON.parse(data); 
+  } catch (error) {
   // TODO: if an exception is raised
   // print the error to the console
   // and return null
+  console.error(error);
+    return null;
+  }
 }
 
 let failData = "<h1>Hello World!</h1>"
